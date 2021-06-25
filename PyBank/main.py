229446061e197a09b2463e551d175dc5886471ae
Karina Hutula <khutula@g.clemson.edu>
2021-06-24 22:19:43 -0400

    add code to find average change in profit/loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,8 @@ import csv
 dates = []
 profits_losses = []
 net_total = 0
+changes = []
+total_change = 0
 
 csvpath = os.path.join("Resources","budget_data.csv")
 
@@ -16,3 +18,10 @@ with open(csvpath, newline="",encoding = 'utf-8') as csvfile:
 
 for i in range(1,len(profits_losses)):
     net_total += int(profits_losses[i])
+    if i > 1:
+        changes.append(int(profits_losses[i])-int(profits_losses[i-1]))
+
+for change in changes:
+    total_change += change
+
+average_change = round(total_change / len(changes), 2)
