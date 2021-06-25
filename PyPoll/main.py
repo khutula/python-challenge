9db4c23053d609da2062618edbc0be863d1ddc5e
Karina Hutula <khutula@g.clemson.edu>
2021-06-25 15:03:51 -0400

    add comments to code for pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,44 +1,67 @@
+# import modules
 import os
 import csv
 
-vote_count = 0
+# set dictionary for recording candidates and votes
 candidates = {}
-vote_percent = 0
-highest_votes = 0
-print_lines = []
 
+# create path for csv file to be read
 csvpath = os.path.join("Resources","election_data.csv")
 
+# open csv file and read through
 with open(csvpath, newline="",encoding = 'utf-8') as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",")
+
+    # each row is looped through: each candidate voted for is stored in a dictionary
     for row in csvreader:
+
+        # if the candidate is already in the dictionary, their vote count is increased by one; else the candidate is added to the dictionary with a vote of 1 (excluding header row)
         if candidates.get(row[2], False):
             candidates[row[2]] += 1
         elif row[2] != "Candidate": 
             candidates[row[2]] = 1
 
+# set variables for recording total vote count & highest number of votes
+vote_count = 0
+highest_votes = 0
+
+# loop through candidates & votes in the dictionary to count total # votes & candidate with highest # votes
 for candidate, votes in candidates.items():
     vote_count += votes
+
+    # if candidate's votes are higher than highest_votes, then winner & highest_votes are updated
     if votes > highest_votes:
         winner = candidate
         highest_votes = votes
 
+# set list for lines to be printed & variable for calculating a candidate's percent of votes
+vote_percent = 0
+print_lines = []
+
+# add starting strings to each item of the print_lines list
 print_lines.append("Election Results")
 print_lines.append("-----------------------------")
 print_lines.append("Total Votes: " + str(vote_count))
 print_lines.append("-----------------------------")
 
+# add more strings to each item of the print_lines list
+# looping through the candidates & votes to determine vote_percent of that candidate & record all variables in a string for print_lines list
 for candidate, votes in candidates.items():
     vote_percent = round(votes/vote_count*100,3)
     print_lines.append(candidate + ": " + str(vote_percent) + "% (" + str(votes) + ")")
 
+# add ending strings to each item of the print_lines list
 print_lines.append("-----------------------------")
 print_lines.append("Winner: " + winner)
 print_lines.append("-----------------------------")
 
+# create the output text file
 output_path = os.path.join("Analysis","pypoll_results.txt")
 
+# write to the outpuut text file the results
 with open(output_path, "w") as txtfile:       
+
+    # loop through all print_lines and print to both the text file and the terminal
     for line in print_lines:
         txtfile.writelines([line+"\n"])
         print(line)
