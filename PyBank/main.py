a016239e044ff88bab081e57294f34f02a63b05d
Karina Hutula <khutula@g.clemson.edu>
2021-06-24 22:13:42 -0400

    add code for finding net profit/loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,11 +4,15 @@ import csv
 
 dates = []
 profits_losses = []
+net_total = 0
 
 csvpath = os.path.join("Resources","budget_data.csv")
 
-with open(csvpath, newline="",encoding = 'utf-8'):
+with open(csvpath, newline="",encoding = 'utf-8') as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",")
     for row in csvreader:
         dates.append(row[0])
         profits_losses.append(row[1])
+
+for i in range(1,len(profits_losses)):
+    net_total += int(profits_losses[i])
