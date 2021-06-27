c004a02ee35768a7b39736a9dd7f978fb9535872
Karina Hutula <khutula@g.clemson.edu>
2021-06-27 19:04:56 -0400

    skip header row in csvreader & adjust following code accordingly

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,6 +13,9 @@ csvpath = os.path.join("Resources","budget_data.csv")
 with open(csvpath, newline="",encoding = 'utf-8') as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",")
 
+    # skip header row
+    header = next(csvreader)
+
     # each row is looped through: months are added to dates list & net profit/loss is added to profits_loss list
     for row in csvreader:
         dates.append(row[0])
@@ -24,12 +27,12 @@ net_total = 0
 # set list for recording net change from month to month
 changes = []
 
-# loop through every entry in the profits_losses list (except the header) & add it to the net_total
-for i in range(1,len(profits_losses)):
+# loop through every entry in the profits_losses list & add it to the net_total
+for i in range(len(profits_losses)):
     net_total += int(profits_losses[i])
 
     # calcualte net change from month (i) to month (i - 1) only if we aren't looking at the first month's data
-    if i > 1:
+    if i > 0:
         changes.append(int(profits_losses[i])-int(profits_losses[i-1]))
 
 # set variable for calculating total net change
@@ -48,10 +51,10 @@ for index, change in enumerate(changes):
     # if the change value a max increase or max decrease in profits, assign value & month to respective variables
     if change > max_increase:
         max_increase = change
-        max_inc_month = dates[index + 2]
+        max_inc_month = dates[index + 1]
     elif change < max_decrease:
         max_decrease = change
-        max_dec_month = dates[index + 2]
+        max_dec_month = dates[index + 1]
 
 # calculate the average monthly change
 average_change = round(total_change / len(changes), 2)
@@ -62,7 +65,7 @@ lines = []
 # add strings to each item of the lines list
 lines.append("Financial Analysis")
 lines.append("-----------------------------")
-lines.append( "Total Months: " + str(len(dates)-1))
+lines.append( "Total Months: " + str(len(dates)))
 lines.append("Total: $" + str(net_total))
 lines.append("Average Change: $" + str(average_change))
 lines.append("Greatest Increase in Profits: " + max_inc_month + " ($" + str(max_increase) + ")")
